6cda7a1a6b36ee60d947227a1d6d21ad3fe0dcea
Kiep13 <cyberpolarka@gmail.com>
2025-04-13 19:52:00 +0200

    initialize project

---
+++ src/main/java/com/cyberapple/followme/FollowmeApplication.java
@@ -0,0 +1,14 @@
+package com.cyberapple.followme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowmeApplication.class, args);
+		System.out.println("This is the start point of great agency website - FollowMe!");
+	}
+
+}
