e24ea8c57c84e01771611615f8ca1b0f0926ccfb
Kiep13 <cyberpolarka@gmail.com>
2025-04-16 13:44:49 +0200

    fm-1 - add excursion page

--- src/main/java/com/cyberapple/followme/mvc/controllers/ExcursionController.java
+++ src/main/java/com/cyberapple/followme/mvc/controllers/ExcursionController.java
@@ -4,6 +4,7 @@ import com.cyberapple.followme.services.ExcursionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ExcursionController {
@@ -20,5 +21,13 @@ public class ExcursionController {
 
         model.addAttribute("excursions", excursions);
         return "dashboard";
+    }
+
+    @GetMapping("/excursions/{id}")
+    public String getDashboard(@PathVariable String id, Model model) {
+        var excursion = excursionService.getExcursionById(id);
+
+        model.addAttribute("excursion", excursion);
+        return "excursion";
     }
 }
