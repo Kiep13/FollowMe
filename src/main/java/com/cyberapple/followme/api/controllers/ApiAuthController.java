7727599a851ce76a2ade18dba3ae1a5f1ff7d01c
Kiep13 <cyberpolarka@gmail.com>
2025-04-26 17:26:11 +0200

    fm-1 - add error handling to request

--- src/main/java/com/cyberapple/followme/api/controllers/ApiAuthController.java
+++ src/main/java/com/cyberapple/followme/api/controllers/ApiAuthController.java
@@ -1,6 +1,5 @@
 package com.cyberapple.followme.api.controllers;
 
-import com.cyberapple.followme.records.LoginRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cyberapple.followme.repositories.UserRepository;
 import com.cyberapple.followme.entities.User;
-
-import java.util.Optional;
+import com.cyberapple.followme.exceptions.InvalidCredentialsException;
+import com.cyberapple.followme.records.LoginRequest;
 
 @RestController
 @AllArgsConstructor
@@ -17,13 +16,8 @@ public class ApiAuthController {
     private final UserRepository userRepository;
 
     @GetMapping("api/auth/login")
-    public Optional<User> login(@RequestBody LoginRequest loginRequest) {
-        Optional<User> user = userRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password());
-
-        if (user.isPresent()) {
-            return user;
-        } else {
-            throw new RuntimeException("Invalid email or password");
-        }
+    public User login(@RequestBody LoginRequest loginRequest) {
+        return userRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password())
+                .orElseThrow(() -> new InvalidCredentialsException("Invalid email or password"));
     }
 }
---
+++ src/main/java/com/cyberapple/followme/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.cyberapple.followme.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.cyberapple.followme.exceptions.InvalidCredentialsException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+}
