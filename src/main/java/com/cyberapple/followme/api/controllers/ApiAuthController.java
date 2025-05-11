03ea96f66723ea74be89518a42940d0ac05ff27c
Kiep13 <cyberpolarka@gmail.com>
2025-05-11 14:53:18 +0200

    fm-1 - api improvements

--- src/main/java/com/cyberapple/followme/api/controllers/ApiAuthController.java
+++ src/main/java/com/cyberapple/followme/api/controllers/ApiAuthController.java
@@ -3,6 +3,7 @@ package com.cyberapple.followme.api.controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cyberapple.followme.repositories.UserRepository;
@@ -11,11 +12,12 @@ import com.cyberapple.followme.exceptions.InvalidCredentialsException;
 import com.cyberapple.followme.records.LoginRequest;
 
 @RestController
+@RequestMapping("api/auth")
 @AllArgsConstructor
 public class ApiAuthController {
     private final UserRepository userRepository;
 
-    @GetMapping("api/auth/login")
+    @GetMapping("/login")
     public User login(@RequestBody LoginRequest loginRequest) {
         return userRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password())
                 .orElseThrow(() -> new InvalidCredentialsException("Invalid email or password"));
--- src/main/java/com/cyberapple/followme/api/controllers/ApiExcursionController.java
+++ src/main/java/com/cyberapple/followme/api/controllers/ApiExcursionController.java
@@ -4,27 +4,27 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import com.cyberapple.followme.dtos.ExcursionDto;
-import com.cyberapple.followme.entities.Excursion;
 import com.cyberapple.followme.entities.Participation;
 import com.cyberapple.followme.services.ExcursionService;
 
 @RestController
+@RequestMapping("api/excursions")
 @AllArgsConstructor
 public class ApiExcursionController {
 
     private final ExcursionService excursionService;
 
-    @GetMapping("api/excursions")
+    @GetMapping("")
     public Iterable<ExcursionDto> getAllExcursions() {
         return excursionService.getAllExcursions();
     }
 
-    @GetMapping("api/excursions/{id}")
+    @GetMapping("/{id}")
     public ExcursionDto getExcursionById(@PathVariable String id) {
         return excursionService.getExcursionById(id);
     }
 
-    @PostMapping("api/excursions/{id}/join")
+    @PostMapping("/{id}/join")
     public void registerForExcursion(@PathVariable String id, @RequestBody Participation participation) {
         excursionService.registerForExcursion(id, participation);
     }
