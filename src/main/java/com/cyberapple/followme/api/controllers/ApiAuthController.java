f5a77d75a3cf6a834bf843522edf8445b79c99d5
Kiep13 <cyberpolarka@gmail.com>
2025-05-26 09:16:26 +0200

    fm-2 - fix end point issues

--- src/main/java/com/cyberapple/followme/api/controllers/ApiAuthController.java
+++ src/main/java/com/cyberapple/followme/api/controllers/ApiAuthController.java
@@ -1,10 +1,7 @@
 package com.cyberapple.followme.api.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cyberapple.followme.repositories.UserRepository;
 import com.cyberapple.followme.entities.User;
@@ -17,7 +14,7 @@ import com.cyberapple.followme.records.LoginRequest;
 public class ApiAuthController {
     private final UserRepository userRepository;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public User login(@RequestBody LoginRequest loginRequest) {
         return userRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password())
                 .orElseThrow(() -> new InvalidCredentialsException("Invalid email or password"));
